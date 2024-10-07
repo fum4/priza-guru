8f09271cb757b10268d9a9b9e1d5be97dd7bf3ce
Alex Andronache <andronache.alex22@gmail.com>
2024-10-07 23:06:40 +0300

    add prize

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,26 @@
-import { useState } from 'react'
-import guru from '/guru.jpg'
+import { useState } from 'react';
+import guru from '/guru.jpg';
+import priza from '/priza_1.webp';
+import './style.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
+    const [prize, setPrize] = useState<{id: number; src: string, position: number}[]>([]);
+
+    const handleClick = () => {
+        setCount((count) => count + 1)
+        setPrize([...prize, { id: Date.now(), src: priza, position: Math.random() * 80 }])
+    }
 
   return (
     <>
-      <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-          <img src={guru} className="logo" alt="Vite logo" style={{ maxHeight: '70%', maxWidth: '80%'}}/>
+      <div className='container'>
+          <img src={guru} className="logo" alt="Guru" />
           <h4>Salut, azi am instalat {count} prize</h4>
-          <button onClick={() => setCount(count + 1)}>Dǎ o prizǎ!</button>
+          <button onClick={handleClick}>Dǎ o prizǎ!</button>
+          {prize.map(priza => (
+              <img key={priza.id} src={priza.src} className="priza" alt="Falling prizǎ" style={{left: `${priza.position}vw`}} />
+          ))}
       </div>
     </>
   )
