be80fa177db929c554ea911ad85e92ef0fb5f2e4
Alex Andronache <andronache.alex22@gmail.com>
2024-10-07 23:12:12 +0300

    add sound

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import { useState } from 'react';
 import guru from '/guru.jpg';
 import priza from '/priza_1.webp';
+import wrenchSound from '/wrench_sound.mp4';
 import './style.css';
 
 function App() {
   const [count, setCount] = useState(0);
-    const [prize, setPrize] = useState<{id: number; src: string, position: number}[]>([]);
+  const [prize, setPrize] = useState<{id: number; src: string, position: number}[]>([]);
+  const [isPlayingSound, setIsPlayingSound] = useState(false);
 
     const handleClick = () => {
-        setCount((count) => count + 1)
-        setPrize([...prize, { id: Date.now(), src: priza, position: Math.random() * 80 }])
+        setCount((count) => count + 1);
+        setPrize([...prize, { id: Date.now(), src: priza, position: Math.random() * 80 }]);
+
+        if (!isPlayingSound) {
+            const audio = new Audio(wrenchSound);
+            audio.play();
+            setIsPlayingSound(true);
+
+            setTimeout(() => setIsPlayingSound(false), 27000);
+        }
     }
 
   return (
