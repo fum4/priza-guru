8d792d6938adaff7a35bbe3de49bfc106c229556
Alex Andronache <andronache.alex22@gmail.com>
2024-10-08 02:19:02 +0300

    spacing

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,18 +1,18 @@
 export const Home = ({ onScreenChange }: { onScreenChange: (screen: string) => void }) => {
     return (
-        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: 20 }}>
+        <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center', gap: 20, height: '100%' }}>
             <div style={{ position: 'relative' }}>
                 <h1 style={{ marginBottom: 0, lineHeight: 0.9, position: 'absolute', bottom: 120, left: 63 }}>Prizǎ</h1>
                 <h1 style={{ marginTop: 0, lineHeight: 0.9, fontSize: 93, fontWeight: 100 }}>Guru</h1>
             </div>
             <div style={{ display: 'flex', flexDirection: 'column', gap: 10, alignItems: 'center'}}>
-                <button 
+                <button
                     onClick={() => onScreenChange('intro')}
                     style={{ width: 150 }}
                 >
                     Story
                 </button>
-                <button 
+                <button
                     onClick={() => onScreenChange('play')}
                     style={{ width: 150 }}
                 >
