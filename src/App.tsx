41b4982be4a8d8942cb8a94a73469159b5862d53
Alex Andronache <andronache.alex22@gmail.com>
2024-10-07 22:33:34 +0300

    update img size

--- src/App.tsx
+++ src/App.tsx
@@ -6,8 +6,8 @@ function App() {
 
   return (
     <>
-      <div style={{ width: '100vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
-          <img src={guru} className="logo" alt="Vite logo" width={500}/>
+      <div style={{ width: '100vw', height: '100vh', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center'}}>
+          <img src={guru} className="logo" alt="Vite logo" style={{ maxHeight: '70%', maxWidth: '80%'}}/>
           <h4>Salut, azi am instalat {count} prize</h4>
           <button onClick={() => setCount(count + 1)}>Dǎ o prizǎ!</button>
       </div>
